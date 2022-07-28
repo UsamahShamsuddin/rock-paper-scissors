af85f52d5c808d4728605fcccfee9545e9e27189
Usamah <usamdsb92@gmail.com>
2022-07-28 19:08:58 +0800

    Add function getComputerChoice

---
+++ index.js
@@ -0,0 +1,15 @@
+// randomly return "rock", "paper" or "scissors"
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+        case 0:
+            return "rock";
+            break;
+        case 1:
+            return "paper";
+            break;
+        case 2:
+            return "scissors";
+            break;
+    }
+}
